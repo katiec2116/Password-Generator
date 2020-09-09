d7d12c04ea944775908deab938cc25fd3d9b1c93
katiec <katiecampbell21@outlook.com>
2020-09-09 14:56:44 -0400

    created variables

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numArray = [];
+var specialArray = [];
+var upperArray = [];
+var lowerArray = [];
+var used = [];
+
+
+function generatePassword(){
+  var pLength = prompt("How many characters do you want your password to be");
+    if (pLength <7 || pLength > 128){
+    }
+    
+    else {
+      alert("Sorry, the password length must be between 8 and 128")
+    }
+
+      var specChar = confirm("Do you want to use special characters?")
+      var caps = confirm("Do you want to use uppercase letters?")
+      var lower = confirm("Do you want to use lower case letters?")
+      var num = confirm("Do you want to use numbers?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return "end password"
+}
 
 // Write password to the #password input
 function writePassword() {
