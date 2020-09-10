7158712c9156c01fa520ac684652aa0859e95a50
katiec <katiecampbell21@outlook.com>
2020-09-10 16:17:38 -0400

    added prompts

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ var numArr = ["1","2","3","4","5","6","7","8","9","0"];
 var specialArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var usedArr=[];
 
+function generatePassword() {
+  var pLength = prompt("How many characters do you want your password to be?");
+
+  var specChar = confirm("Do you want to use special characters?");
+
+  var upper = confirm("Do you want to use uppercase letters?");
+
+  var lower = confirm("Do you want to use lower case letters?");
+  
+  var num = confirm("Do you want to use numbers?");
 
 
 // Write password to the #password input
