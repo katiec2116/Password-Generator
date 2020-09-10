0dd17f8329caf1c6ad5ec506567570e030594e2c
katiec <katiecampbell21@outlook.com>
2020-09-10 18:10:22 -0400

    empty array at beginning of function

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var numArr = ["1","2","3","4","5","6","7","8","9","0"];
 var specialArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var usedArr=[];
 
 function generatePassword() {
+      var usedArr=[];
       var pLength = prompt("How many characters do you want your password to be?");
       while(pLength < 7 || pLength > 128) {
         alert("Sorry, the password length must be between 8 and 128 character.");
@@ -17,36 +17,36 @@ function generatePassword() {
       }
       if (pLength > 7 && pLength < 129) {
 
-      var specChar = confirm("Do you want to use special characters?");
-        if(specChar){
-          usedArr.push(...specialArr);
-          console.log(usedArr);
-        }
+          var specChar = confirm("Do you want to use special characters?");
+            if(specChar){
+              usedArr.push(...specialArr);
+              console.log(usedArr);
+            }
 
-      var upper = confirm("Do you want to use uppercase letters?");
-      if(upper){
-        usedArr.push(...upperArr);
-        console.log(usedArr);
-      }
+          var upper = confirm("Do you want to use uppercase letters?");
+          if(upper){
+            usedArr.push(...upperArr);
+            console.log(usedArr);
+          }
 
-      var lower = confirm("Do you want to use lower case letters?");
-      if(lower){
-        usedArr.push(...lowerArr);
-        console.log(usedArr);
-      }
+          var lower = confirm("Do you want to use lower case letters?");
+          if(lower){
+            usedArr.push(...lowerArr);
+            console.log(usedArr);
+          }
 
-      var num = confirm("Do you want to use numbers?");
-      if(num){
-        usedArr.push(...numArr);
-        console.log(usedArr);
-      }
+          var num = confirm("Do you want to use numbers?");
+          if(num){
+            usedArr.push(...numArr);
+            console.log(usedArr);
+          }
 
-      var password = [];
-              for (var p = 0; p < pLength; p++){
+          var password = [];
+            for (var p = 0; p < pLength; p++){
               password.push(usedArr[Math.floor(Math.random() * usedArr.length)]);  
-              }
-              return password.join("")
-  }
+            }
+        return password.join("")
+      }
 }
 // Write password to the #password input
 function writePassword() {
